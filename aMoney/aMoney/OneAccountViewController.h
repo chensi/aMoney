832caa71bea5644e02a1fee2e25ce213c751e731
luochunlei <luo.chunlei@gmail.com>
2012-07-01 22:16:54 +0800

    添加 新加或者修改 一个账单的画面。 只有空壳类。

---
+++ aMoney/aMoney/OneAccountViewController.h
@@ -0,0 +1,17 @@
+//
+//  OneAccountViewController.h
+//  aMoney
+//
+//  Created by 春雷 罗 on 12-7-1.
+//  Copyright (c) 2012年 private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 
+// 添加或者修改一个账单的界面。
+// 总结就是显示一个账单的详细信息。
+//
+@interface OneAccountViewController : UIViewController
+
+@end
---
+++ aMoney/aMoney/OneAccountViewController.m
@@ -0,0 +1,44 @@
+//
+//  OneAccountViewController.m
+//  aMoney
+//
+//  Created by 春雷 罗 on 12-7-1.
+//  Copyright (c) 2012年 private. All rights reserved.
+//
+
+#import "OneAccountViewController.h"
+
+@interface OneAccountViewController ()
+
+@end
+
+@implementation OneAccountViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
